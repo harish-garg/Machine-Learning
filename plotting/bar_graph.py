ae1024d37d6cd20299d2fdf8f6bee3fe4dde6cb1
Harish Garg <harish.garg@gmail.com>
2016-08-26 15:35:35 +0530

    Some plotting exerices

---
+++ plotting/bar_graph.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+
+names = ["McDonalds", "Burger King", "Wendys", "Subway"]
+patrons = [10000, 5000, 5000, 7500]
+x = [0, 1, 2, 3]
+
+plt.bar(x, patrons)
+plt.show()
---
+++ plotting/line_chart.py
@@ -0,0 +1,7 @@
+import matplotlib.pyplot as plt
+
+age = [5, 10, 15, 20, 25]
+height = [25, 45, 65, 75, 75]
+
+plt.plot(age, height)
+plt.show()
